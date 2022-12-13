a2f7dc80ef1de87fe94d7df7ed6deaa9ac4beeb8
M10T <milan.tenn@gmail.com>
2022-12-13 09:31:12 -0500

    Improved interface.

--- src/main/resources/trace.js
+++ src/main/resources/trace.js
@@ -10,29 +10,28 @@ function renderQuestions(problemsString) {
     }
     // console.log(problems2dArr);
     // console.log(answersArr);
-    document.getElementsByTagName("body")[0].innerHTML += `<form id='problems' onsubmit = "return answerSubmit(` + answersArr + `)"></form>`;
     for (let i=0; i < problems2dArr.length; i++) {
+        let currentForm = document.createElement("form")
+        currentForm.id = `problem${i}`
+        currentForm.onsubmit = function() {return answerSubmit(answersArr, i)}
         let problem = problems2dArr[i]
-        // console.log(problem);
-        document.getElementById(`problems`).innerHTML += "Code:" + "<br>";
-        document.getElementById(`problems`).innerHTML += problem[0].replace(/\n/g, "<br>").replace(/\t/g, "&emsp;"); 
-        document.getElementById(`problems`).innerHTML += "<p>What is displayed line " + problem[5] + "?</p>";
-        document.getElementById(`problems`).innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v1'>"+ problem[1] + "</input><br>";
-        document.getElementById(`problems`).innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v2'>"+ problem[2] + "</input><br>";
-        document.getElementById(`problems`).innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v3'>"+ problem[3] + "</input><br>";
-        document.getElementById(`problems`).innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v4'>"+ problem[4] + "</input><br><br>";
+        currentForm.innerHTML += `<b>Problem ${i+1}:</b><br/>`;
+        currentForm.innerHTML += problem[0].replace(/\n/g, "<br>").replace(/\t/g, "&emsp;"); 
+        currentForm.innerHTML += "<p>What is displayed line " + problem[5] + "?</p>";
+        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v1'>"+ problem[1] + "</input><br>";
+        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v2'>"+ problem[2] + "</input><br>";
+        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v3'>"+ problem[3] + "</input><br>";
+        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v4'>"+ problem[4] + "</input><br><br>";
+        currentForm.innerHTML += "<input type='submit'></input>"
+        document.getElementsByTagName("body")[0].appendChild(currentForm)
     }
-    document.getElementById(`problems`).innerHTML += "<input type='submit'></input><br>";
 }
 
-function answerSubmit(answersArr) {
-    let windowsMessage = "";
-    for (let i=0; i < answersArr.length; i++){
-        let actual = document.querySelector("input[name='answers" + i + "']:checked").value;
-        if (actual == answersArr[i]) windowsMessage += i + ". Correct"
-        else windowsMessage += i + ". Incorrect"
-    }
-    console.log(windowsMessage);
+function answerSubmit(answersArr, i) {
+    let windowsMessage;
+    let actual = document.querySelector("input[name='answers" + i + "']:checked").value;
+    if (actual == answersArr[i]) windowsMessage = "Correct"
+    else windowsMessage = "Incorrect"
     window.alert(windowsMessage);
     return false;
 }
