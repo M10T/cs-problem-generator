69b510ff1a7570eac9b94ef2d106e691bd02f26f
AutumnQiu99 <autumn.t.qiu@gmail.com>
2023-01-10 10:42:08 -0500

    added free response

--- src/main/resources/trace.js
+++ src/main/resources/trace.js
@@ -1,5 +1,6 @@
-function renderQuestions(problemsString) {
-    //console.log(problemsString);
+function renderQuestions(problemsString, responseType) {
+    console.log(responseType);
+
     let problems = problemsString.split("|||");
     let problems2dArr = [];
     let answersArr = []; 
@@ -8,30 +9,42 @@ function renderQuestions(problemsString) {
         problems2dArr.push(problem);
         answersArr.push('v' + problem[6]);
     }
-    // console.log(problems2dArr);
-    // console.log(answersArr);
+  
     for (let i=0; i < problems2dArr.length; i++) {
         let currentForm = document.createElement("form")
         currentForm.id = `problem${i}`
-        currentForm.onsubmit = function() {return answerSubmit(answersArr, i)}
         let problem = problems2dArr[i]
         currentForm.innerHTML += `<b>Problem ${i+1}:</b><br/>`;
         currentForm.innerHTML += problem[0].replace(/\n/g, "<br>").replace(/\t/g, "&emsp;"); 
         currentForm.innerHTML += "<p>What is displayed line " + problem[5] + "?</p>";
-        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v1'>"+ problem[1] + "</input><br>";
-        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v2'>"+ problem[2] + "</input><br>";
-        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v3'>"+ problem[3] + "</input><br>";
-        currentForm.innerHTML += "<input type='radio' name = 'answers" + i + "' value = 'v4'>"+ problem[4] + "</input><br><br>";
+        if (responseType == "multipleChoice") {
+            currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v1'>"+ problem[1] + "</input><br>";
+            currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v2'>"+ problem[2] + "</input><br>";
+            currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v3'>"+ problem[3] + "</input><br>";
+            currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v4'>"+ problem[4] + "</input><br><br>";
+            currentForm.onsubmit = function() {return answerSubmit(answersArr, i, responseType)}
+        } else if (responseType == "freeResponse") {
+            currentForm.innerHTML += "<input type='text' name='answers" + i + "'></input><br>";
+            currentForm.onsubmit = function() {return answerSubmit([problem[problem[6]]], i, responseType)}
+        }
         currentForm.innerHTML += "<input type='submit'></input>"
         document.getElementsByTagName("body")[0].appendChild(currentForm)
     }
 }
 
-function answerSubmit(answersArr, i) {
+function answerSubmit(answersArr, i, responseType) {
     let windowsMessage;
-    let actual = document.querySelector("input[name='answers" + i + "']:checked").value;
-    if (actual == answersArr[i]) windowsMessage = "Correct"
-    else windowsMessage = "Incorrect"
-    window.alert(windowsMessage);
+    let actual;
+    if (responseType == "multipleChoice") {
+        actual = document.querySelector("input[name='answers" + i + "']:checked").value;
+        if (actual == answersArr[i]) windowsMessage = "Correct"
+        else windowsMessage = "Incorrect"
+        window.alert(windowsMessage);
+    } else if (responseType == "freeResponse") {
+        actual = document.querySelector("input[name='answers" + i + "']").value;
+        if (actual == answersArr[0]) windowsMessage = "Correct"
+        else windowsMessage = "Incorrect"
+        window.alert(windowsMessage);
+    }
     return false; 
 }
--- src/main/scala/MinimalApplication.scala
+++ src/main/scala/MinimalApplication.scala
@@ -25,7 +25,15 @@ object MinimalApplication extends cask.MainRoutes{
           label(attr("for"):="randomCode")("Random Code Generator"),
           br(),
           input(attr("type"):="radio", attr("id"):="mathCode", attr("name"):="problemType", attr("value"):="mathCode"),
-          label(attr("for"):="randomCode")("Random Math Code Generator"),
+          label(attr("for"):="mathCode")("Random Math Code Generator"),
+          br(),
+          br(), 
+          input(attr("type"):="radio", attr("id"):="multipleChoice", attr("name"):="responseType", attr("value"):="multipleChoice"),
+          label(attr("for"):="multipleChoice")("Multiple Choice"),
+          br(),
+          input(attr("type"):="radio", attr("id"):="freeResponse", attr("name"):="responseType", attr("value"):="freeResponse"),
+          label(attr("for"):="freeResponse")("Free Response"),
+          br(),
           br(),
           input(attr("type"):="number", attr("id"):="numberOfProblems", attr("name"):="numberOfProblems", attr("value"):="1", attr("min"):="1"),
           input(attr("type"):="submit", attr("id"):="submit"),
@@ -35,9 +43,9 @@ object MinimalApplication extends cask.MainRoutes{
   }
 
   @cask.get("/problemTypeSelector")
-  def problemTypeSelector(problemType: String, numberOfProblems: Int) = problemType match
+  def problemTypeSelector(problemType: String, responseType: String, numberOfProblems: Int) = problemType match
     case "randomCode" => cask.Response(randomCode())
-    case "trace" => cask.Response(trace(numberOfProblems))
+    case "trace" => cask.Response(trace(responseType, numberOfProblems))
     case "mathCode" => cask.Response(mathTest())
     case _ => cask.Response(html(), 401)
   
@@ -81,7 +89,7 @@ object MinimalApplication extends cask.MainRoutes{
   def staticFileRoutes() = "/src/main/resources"
 
   @cask.get("/trace")
-  def trace(numberOfProblems: Int = 1) = {
+  def trace(responseType: String, numberOfProblems: Int = 1) = {
     val r = scala.util.Random
     var problems = Array.ofDim[String](numberOfProblems, 7)
 
@@ -97,7 +105,7 @@ object MinimalApplication extends cask.MainRoutes{
 
       problems(i-1)(0) = translated
       problems(i-1)(5) = line.toString()
-      problems(i-1)(6) = answerIndex.toString()
+      problems(i-1)(6) = answerIndex.toString() // 7th element = line number of answer 
 
       for(j <- 1 to 4){
         if (j == answerIndex) {
@@ -117,7 +125,7 @@ object MinimalApplication extends cask.MainRoutes{
     }
 
     html(
-      body(onload:=s"renderQuestions(`$problemsString`)")(
+      body(onload:=s"renderQuestions(`$problemsString`, `$responseType`)")(
         script(src := "/static/trace.js")
       )
     )
