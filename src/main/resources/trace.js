012862efce42ff7a43c148d8607687e8a2e4776b
AutumnQiu99 <autumn.t.qiu@gmail.com>
2023-01-12 09:53:46 -0500

    added mixed response types

--- src/main/resources/trace.js
+++ src/main/resources/trace.js
@@ -1,6 +1,8 @@
 function renderQuestions(problemsString, responseType) {
     console.log(responseType);
 
+    let responseTypesBank = ["multipleChoice", "freeResponse"]
+
     let problems = problemsString.split("|||");
     let problems2dArr = [];
     let answersArr = []; 
@@ -11,21 +13,27 @@ function renderQuestions(problemsString, responseType) {
     }
   
     for (let i=0; i < problems2dArr.length; i++) {
+        let currentResponseType; 
+        if (responseType == "mixedResponseTypes") {
+            currentResponseType = responseTypesBank[Math.floor(Math.random() * 2)];
+        } else {
+            currentResponseType = responseType;
+        }
         let currentForm = document.createElement("form")
         currentForm.id = `problem${i}`
         let problem = problems2dArr[i]
         currentForm.innerHTML += `<b>Problem ${i+1}:</b><br/>`;
         currentForm.innerHTML += problem[0].replace(/\n/g, "<br>").replace(/\t/g, "&emsp;"); 
         currentForm.innerHTML += "<p>What is displayed line " + problem[5] + "?</p>";
-        if (responseType == "multipleChoice") {
+        if (currentResponseType == "multipleChoice") {
             currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v1'>"+ problem[1] + "</input><br>";
             currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v2'>"+ problem[2] + "</input><br>";
             currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v3'>"+ problem[3] + "</input><br>";
             currentForm.innerHTML += "<input type='radio' name='answers" + i + "' value = 'v4'>"+ problem[4] + "</input><br><br>";
-            currentForm.onsubmit = function() {return answerSubmit(answersArr, i, responseType)}
-        } else if (responseType == "freeResponse") {
+            currentForm.onsubmit = function() {return answerSubmit(answersArr, i, currentResponseType)}
+        } else if (currentResponseType == "freeResponse") {
             currentForm.innerHTML += "<input type='text' name='answers" + i + "'></input><br>";
-            currentForm.onsubmit = function() {return answerSubmit([problem[problem[6]]], i, responseType)}
+            currentForm.onsubmit = function() {return answerSubmit([problem[problem[6]]], i, currentResponseType)}
         }
         currentForm.innerHTML += "<input type='submit'></input>"
         document.getElementsByTagName("body")[0].appendChild(currentForm)
